7e7c9e68b167fc5dad2a36c5e4a58c7c76930e18
emines0 <emin.sefa1@gmail.com>
2023-02-11 19:14:37 +0100

    handleSubmit functionality, handle error, create SingleColor component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,20 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    try {
+      setError(false)
+      let colors = new Values(color).all(10)
+      /*
+       * get 10 tint/ 10 shade values from value.js
+       */
+      setList(colors)
+      /*
+       * set retrieved values to the list of color values
+       */
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
   }
 
   return (
@@ -26,6 +39,10 @@ function App() {
              * onChange => setColor to the value which user type in the input field
              */
             placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+            /*
+             * If there is an error set class error if not set class null
+             */
           />
           <button className='btn' type='submit'>
             submit
@@ -33,7 +50,14 @@ function App() {
         </form>
       </section>
       <section className='colors'>
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          console.log(color)
+          return <SingleColor key={index} {...color} index={index} />
+          /*
+           * {...color} passing all props of object color to SingleColor component
+           * index => prop
+           */
+        })}{' '}
       </section>
     </>
   )
